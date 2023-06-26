d598fbbb0d5982d9735792579c68259adca61c44
LelouchL2 <cgcrew001@gmail.com>
2023-06-26 23:28:24 +0530

    fix: Create subreddit error handling

--- src/app/api/subreddit/route.ts
+++ src/app/api/subreddit/route.ts
@@ -1,6 +1,7 @@
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { SubredditValidator } from "@/lib/validators/subreddit";
+import { z } from "zod";
 
 export async function POST(req: Request) {
   try {
@@ -13,6 +14,36 @@ export async function POST(req: Request) {
     const body = await req.json();
     const { name } = SubredditValidator.parse(body);
 
-    const subredditExists = await db.subreddit;
-  } catch (error) {}
+    const subredditExists = await db.subreddit.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (subredditExists) {
+      return new Response("Subreddit already exist", { status: 409 });
+    }
+
+    const subreddit = await db.subreddit.create({
+      data: {
+        name,
+        creatorId: session.user.id,
+      },
+    });
+
+    await db.subscription.create({
+      data: {
+        userId: session.user.id,
+        subredditId: subreddit.id,
+      },
+    });
+
+    return new Response(subreddit.name);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(error.message, { status: 422 });
+    }
+
+    return new Response("Could not create subreddit", { status: 500 });
+  }
 }
