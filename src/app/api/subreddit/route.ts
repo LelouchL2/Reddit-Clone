3c7e111c1cf888dd7bad6a66272e92f22dddc16b
LelouchL2 <cgcrew001@gmail.com>
2023-06-27 18:30:17 +0530

    feat: Finish create community/subreddit

--- src/app/api/subreddit/route.ts
+++ src/app/api/subreddit/route.ts
@@ -8,12 +8,13 @@ export async function POST(req: Request) {
     const session = await getAuthSession();
 
     if (!session?.user) {
-      return new Response("unauthorized", { status: 401 });
+      return new Response("Unauthorized", { status: 401 });
     }
 
     const body = await req.json();
     const { name } = SubredditValidator.parse(body);
 
+    // check if subreddit already exists
     const subredditExists = await db.subreddit.findFirst({
       where: {
         name,
@@ -21,9 +22,10 @@ export async function POST(req: Request) {
     });
 
     if (subredditExists) {
-      return new Response("Subreddit already exist", { status: 409 });
+      return new Response("Subreddit already exists", { status: 409 });
     }
 
+    // create subreddit and associate it with the user
     const subreddit = await db.subreddit.create({
       data: {
         name,
@@ -31,6 +33,7 @@ export async function POST(req: Request) {
       },
     });
 
+    // creator also has to be subscribed
     await db.subscription.create({
       data: {
         userId: session.user.id,
--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
+import { useCustomToasts } from "@/hooks/use-custom-toast";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { CreateSubredditPayload } from "@/lib/validators/subreddit";
+import { toast } from "@/hooks/use-toast";
 
 const Page = () => {
   const [input, setInput] = useState<string>("");
   const router = useRouter();
+  const { loginToast } = useCustomToasts();
 
   const { mutate: createCommunity, isLoading } = useMutation({
     mutationFn: async () => {
@@ -18,6 +21,39 @@ const Page = () => {
       };
       const { data } = await axios.post("/api/subreddit", payload);
       return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "Subreddit already exists",
+            description: "Please choose a different name",
+            variant: "destructive",
+          });
+        }
+
+        if (err.response?.status === 422) {
+          return toast({
+            title: "Invaild subreddit name",
+            description: "Please choose a name between 2 to 21 characters.",
+            variant: "destructive",
+          });
+        }
+
+        if (err.response?.status === 401) {
+          console.log("Unauthorized");
+          return loginToast();
+        }
+      }
+
+      toast({
+        title: "There was an Error.",
+        description: "Could not create a subreddit",
+        variant: "destructive",
+      });
+    },
+    onSuccess(data) {
+      router.push(`/r/${data}`);
     },
   });
 
