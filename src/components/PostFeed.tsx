dd3edfbcc44990a6ad27e8ef03a63e5594097762
LelouchL2 <cgcrew001@gmail.com>
2023-07-01 11:10:54 +0530

    feat: Infinite post scroll

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 import { ExtendedPost } from "@/types/db";
 import { useIntersection } from "@mantine/hooks";
 import { useInfiniteQuery } from "@tanstack/react-query";
@@ -39,6 +39,12 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
       initialData: { pages: [initialPosts], pageParams: [1] },
     }
   );
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
 
   const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
 
