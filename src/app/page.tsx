f132edf2350db1244b61b81e4253dc0ebd81675f
LelouchL2 <cgcrew001@gmail.com>
2023-07-02 13:34:56 +0530

    feat: Add change username

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,24 +43,3 @@ export default async function Home() {
     </>
   );
 }
-
-function PostVoteShell() {
-  return (
-    <div className="flex items-center flex-col pr-6 w-20">
-      {/* upvote */}
-      <div className={buttonVariants({ variant: "ghost" })}>
-        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
-      </div>
-
-      {/* score */}
-      <div className="text-center py-2 font-medium text-sm text-zinc-900">
-        <Loader2 className="h-3 w-3 animate-spin" />
-      </div>
-
-      {/* downvote */}
-      <div className={buttonVariants({ variant: "ghost" })}>
-        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
-      </div>
-    </div>
-  );
-}
--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,15 +1,12 @@
 "use client";
 import {
   DropdownMenu,
-  DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
-} from "./ui/DropdownMenu.tsx";
+  DropdownMenuTrigger,
+} from "@/components/ui/DropdownMenu";
 import { User } from "next-auth";
-import { FC } from "react";
 import UserAvatar from "./UserAvatar";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
@@ -18,7 +15,7 @@ interface UserAccountNavProps {
   user: Pick<User, "name" | "image" | "email">;
 }
 
-const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+const UserAccountNav = ({ user }: UserAccountNavProps) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -33,7 +30,7 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
       <DropdownMenuContent className="bg-white" align="end">
         <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-1 leading-none">
-            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.name && <p className="font-medium">{user.email}</p>}
             {user.email && (
               <p className="w-[200px] truncate text-sm text-zinc-700">
                 {user.email}
