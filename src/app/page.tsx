eb4b154e480ec6ba180f67e9e2c0234142df287f
LelouchL2 <cgcrew001@gmail.com>
2023-07-13 10:06:40 +0530

    fix: bg change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
+      <div className="grid grid-cols-1 bg-black md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
         {/* Custom feed */}
         {/* @ts-expect-error server components*/}
         {session ? <CustomFeed /> : <GeneralFeed />}
